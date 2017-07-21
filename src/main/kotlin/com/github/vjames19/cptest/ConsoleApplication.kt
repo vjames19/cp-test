4d11198cc728a861c6a6078d39c061df7acba08a
Victor J. Reventos <victor.reventos1@gmail.com>
2017-07-21 18:05:51 -0500

    Using the injector in the console application.

--- src/main/kotlin/com/github/vjames19/cptest/ConsoleApplication.kt
+++ src/main/kotlin/com/github/vjames19/cptest/ConsoleApplication.kt
@@ -1,19 +1,17 @@
 package com.github.vjames19.cptest
 
-import com.github.vjames19.cptest.network.InMemoryNetworkService
-import com.github.vjames19.cptest.parser.UserParser
+import com.github.vjames19.cptest.api.di.ServiceModule
+import com.github.vjames19.cptest.network.NetworkService
+import com.google.inject.Guice
 
 /**
  * Created by victor.reventos on 7/19/17.
  */
 fun main(args: Array<String>) {
-    val parser = UserParser()
-    val usersReader = UserParser::class.java.getResourceAsStream("/Person.txt").bufferedReader()
-    val connectionsReaders = UserParser::class.java.getResourceAsStream("/Relationship.txt").bufferedReader()
-    val network = parser.parse(usersReader, connectionsReaders).map { it.id to it }.toMap()
-    val networkService = InMemoryNetworkService(network)
-    val degree = 2
+    val injector = Guice.createInjector(ServiceModule)
+    val networkService = injector.getInstance(NetworkService::class.java)
 
+    val degree = 2
     println("How many total connections  Lucas(id=4) has? ${networkService.numberOfConnections(4, degree)}")
     val whoCanIntroduce = networkService.whoCanIntroduce(4, 62, degree)
     println("Who can introduce Lucas(id=4) to Adam(id=62)?  $whoCanIntroduce for a total of ${whoCanIntroduce.size} connections")
